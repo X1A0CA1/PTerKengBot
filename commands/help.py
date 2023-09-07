85baeb5eac046e54e7b4d1f4be53f60ef201cfbe
xiaocai <52635310+X1A0CA1@users.noreply.github.com>
2023-09-07 23:54:04 +0800

    update(help.py) 更新帮助信息，更新版本

--- commands/help.py
+++ commands/help.py
@@ -4,23 +4,23 @@ import log
 from config import BOT_NAME, HELP_DELETE_TIME
 from utils import reply_and_delay_delete
 
+HELP_MESSAGE = f"""```**PTerKengBot HELP**
+/help@{BOT_NAME}           显示此帮助信息
+/ping@{BOT_NAME}           查看机器人存活状态
+/stats@{BOT_NAME}       *  查看站点统计信息
+/notice_me@{BOT_NAME}   *  获取坑位通知频道链接
+/flush@{BOT_NAME}       !  强制刷新数据
+/eval@{BOT_NAME}        !  ~~调教 bot 使用的命令~~
 
-HELP_MESSAGE = (
-    f"**PTerKengBot HELP**\n\n"
-    f"`/help@{BOT_NAME}`    显示此帮助信息\n"
-    f"`/stats@{BOT_NAME}`    `*` 查看站点统计信息\n"
-    f"`/ping@{BOT_NAME}`    查看机器人存活状态\n"
-    f"`/flush@{BOT_NAME}`    `!` 强制刷新数据\n"
-    f"`/notice_me@{BOT_NAME}`    `*` 获取坑位通知频道链接\n"
-    f"`/eval@{BOT_NAME}`    `!` 奇怪的东西\n\n"
-    f"`/forward_mode@{BOT_NAME} [on|off]`    `!P` 自动转发接下来私聊 BOT 的消息到猫站群\n"
-    f"带有 `!` 的命令仅管理员(被认证的机器人管理员、非匿名群组管理员）可用，皮套人仅可在群组内使用，无法在私聊使用；\n"
-    f"带有 `*` 的命令仅可以在猫站群内使用。\n"
-    f"带有 `P` 的命令仅可以在私聊内使用。\n\n"
-    
-    f"**机器人带有黑名单、日志收集，请勿滥用**。\n@PterKengBot 不是官方所编写、维护的的机器人。\n\n"
-    f"v1.4.0@PlanTech"
-)
+/forward_mode on/off   !P  自动转发接下来私聊 BOT 的消息到猫站群
+
+带有 ! 的命令仅管理员(被认证的机器人管理员、非匿名群组管理员）可用，皮套人仅可在群组内使用，无法在私聊使用；
+带有 * 的命令仅可以在猫站群内使用。
+带有 P 的命令仅可以在私聊内使用。
+
+**机器人带有黑名单、日志收集，请勿滥用**。\n@PterKengBot 不是官方所编写、维护的的机器人。
+v1.4.1@PlanTech"
+"""
 
 
 @Client.on_message(filters.command('help'))
