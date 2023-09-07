af6bdc8da81cc2918c080111303c61032be4ac58
xiaocai <52635310+X1A0CA1@users.noreply.github.com>
2023-09-07 23:47:15 +0800

    perf(forward_mode.py) 排除私聊中开头为/的消息。

--- commands/forward_mode.py
+++ commands/forward_mode.py
@@ -42,12 +42,9 @@ async def reply_mode_command(_, message):
         await message.reply("消息转发已关闭")
 
 
-@Client.on_message(filters.private & ~filters.me)
+@Client.on_message(filters.private & ~filters.me & ~filters.regex(r"^/"))
 async def forward_message(_, message: Message):
     await check_required(message, admin_required=True)
-    if message.text:
-        if message.text.startswith("/"):
-            return
     user_id = message.from_user.id
     if user_id in user_forwarding_enabled and user_forwarding_enabled[user_id]:
         await message.copy(chat_id=WORK_CHAT)
